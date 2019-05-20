2f443251d1675e1f506d2b9ac03269f87ba292ef
ridolud <faridho.lutfi@gmail.com>
2019-05-20 10:58:10 +0700

    Initial Commit

---
+++ fear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fear
+//
+//  Created by Faridho Luedfi on 20/05/19.
+//  Copyright © 2019 ridolud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
